7504559c1db0e89a1868c09685ba8f5ffbb7dd7b
EvgeniyGrokholskiy <grokholskiy@gmail.com>
2022-01-13 20:52:39 +0500

    build

---
+++ src/firebase/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDTLstUk3KREK9GfCM4uh-cMVQxnQjLkrk",
+    authDomain: "web-pack-product-test.firebaseapp.com",
+    projectId: "web-pack-product-test",
+    storageBucket: "web-pack-product-test.appspot.com",
+    messagingSenderId: "384860722185",
+    appId: "1:384860722185:web:451e0345c2ab444294d4d8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
